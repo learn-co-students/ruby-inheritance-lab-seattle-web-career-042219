1436990ec0fb8a4d39f0df390fdb72fc3341986a
jkshareef <jamalkshareef@gmail.com>
2019-04-26 14:13:22 -0700

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+  @@all = []
+
+  def initialize
+    @knowledge = []
+    @@all << self
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
+
 
 end
